6c752ed7b508f407439ded75be975d5f04332e55
DaisyWang2002 <60764164+DaisyWang2002@users.noreply.github.com>
2026-07-18 08:08:20 +0000

    Add files via upload

---
+++ Script/小红书字体加大.user.js
@@ -0,0 +1,31 @@
+
+// ==UserScript==
+// @name         小红书字体加大
+// @namespace    http://tampermonkey.net/
+// @version      0.1.0
+// @description  小红书各个页面字体加大
+// @author       Leslie
+// @match        https://www.xiaohongshu.com/*
+// @match        https://xhslink.com/*
+// @grant        none
+// @run-at       document-idle
+// @license MIT
+
+
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+    addGlobalStyle(".code {font-size: 20px !important}");
+})();
